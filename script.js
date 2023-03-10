a3e9f8c0a99b601129c3697ea1d5ef647c40ba80
Shivam chaudhary <104071079+Shvarag@users.noreply.github.com>
2023-03-10 09:30:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let typed = new Typed(".multiple-text",{
+    strings:["Programmer","Aspiring Web Developer","UI/UX Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
